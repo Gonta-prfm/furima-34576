07466d3c528705581c4a364665abf57513d1a78d
Gonta-prfm <k.0426.s.46@icloud.com>
2021-03-02 11:35:43 +0900

    JavaScriptで販売手数料や販売利益の表示を変更

---
+++ app/javascript/items/price.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDomValue = (Math.floor(inputValue * 0.1));
+    addTaxDom.innerHTML = addTaxDomValue;
+
+    const salesPrice = document.getElementById("profit");
+    salesPrice.innerHTML = (Math.floor(inputValue - addTaxDomValue));
+   });
+  });
