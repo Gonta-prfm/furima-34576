1fcbe753c7e0ea49db6daf48127821177ec33325
Gonta-prfm <k.0426.s.46@icloud.com>
2021-03-02 11:37:22 +0900

    itemモデル単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name               {Faker::Name.name}
+    description        {Faker::Lorem.sentence}
+    price              {1500}
+    category_id        {2}
+    status_id          {2}
+    shopping_charge_id {2}
+    shopping_area_id   {2}
+    days_to_shop_id    {2}
+
+    after(:build) do |product|
+      product.image.attach(io: File.open('public/image/test_image.png'), filename: 'test_image.png')
+    end
+    association :user
   end
 end
