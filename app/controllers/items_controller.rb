e0fa5a2f7fcd68642f1bcdcfc65098b8a66d3069
Gonta-prfm <k.0426.s.46@icloud.com>
2021-03-03 11:21:20 +0900

    重複していたコードをメソッドでまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  before_action :set_item,           only: [:edit, :show, :update]
-  before_action :move_to_index,      only: [:edit, :update]
+  before_action :set_item,           only: [:edit, :show, :update, :destroy]
+  before_action :move_to_index,      only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -35,9 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.user == current_user
-    if item.destroy
+    if @item.destroy
       redirect_to root_path
     else
       render :show
