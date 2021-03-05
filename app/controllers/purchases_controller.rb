8cec9a8fd8c0fdd7251721e2365c3e9c5a96fcfd
Gonta-prfm <k.0426.s.46@icloud.com>
2021-03-05 16:25:38 +0900

    商品購入機能で住所、購入履歴がDBに保存されるよう実装

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,37 @@
+class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item,   only: [:index, :create]
+  before_action :move_index, only: [:index, :create]
+
+  def index
+    @purchase_street_address = PurchaseStreetAddress.new
+  end
+
+  def create
+    @purchase_street_address = PurchaseStreetAddress.new(private_params)
+    if @purchase_street_address.valid?
+      @purchase_street_address.save
+      redirect_to root_path
+    else 
+      render :index
+    end
+  end
+
+    private
+    def private_params
+      params.require(:purchase_street_address).permit(:postal_code, :prefecture_id, :municipality, :address, :buliding_name, 
+                     :phone_number,).merge(user_id: current_user.id, item_id: params[:item_id])
+    end
+
+    def set_item
+      @item = Item.find(params[:item_id])
+    end
+
+    def move_index
+      if current_user.id == @item.user_id || @item.purchase.present?
+        redirect_to root_path
+      end
+    end
+end
+
+
